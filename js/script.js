3f9e6c2f8ba0b6e5d91345826ab49039b19b56c2
Mftr96 <matteo.federici96@gmail.com>
2024-04-19 16:39:51 +0200

    stampo numeri in pagina

--- js/script.js
+++ js/script.js
@@ -3,28 +3,40 @@
 //passi da seguire :
 // definire il for con i tre componenti in parentesi
 //definire cosa esegue ad ogni ciclo(console log numero )
-// se i= multiplo 3 mi stampa fizz e non 3
-// definisco la condizione di multiplo di 3 
-//definisco la condizione di multiplo di 5 
-//definisco la condizione di quando è sia il caso 1 che 2 
 // definisco la condizione di quando non vengono soddisfatti i requisiti 1 e 2
+// voglio provare a stampare la lista 
+
+//richiamo il div come costante 
+//aggiungo alla costante tramite innerhtml
+
+
+
 
 for(let i=1;i<=100;i= i+ 1){
-    
-    if(i % 3==0 && i % 5==0){
-       console.log("fizzbuzz");        
-   }
-  
+    let lista = document.getElementById("contenitore-numeri");
+    const element =`<li class="box box--$(i)"></li>` ;
+    lista.innerHTML+=element+i;
 
+
+
+    if(i % 3==0 && i % 5==0){
+        console.log("fizzbuzz");        
+    }
+    
+    
     else if(i % 3 == 0) {
         console.log("fizz" );
     }
-
+    
     else if(i % 5 ==0){
         console.log("buzz");
     }
-
+    
     else{
         console.log(i)
     }
+    
+    
+  
+
 }
