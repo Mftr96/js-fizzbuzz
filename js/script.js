f21c443efa0256ca70e1ee86a6a860bcae2afd6e
Mftr96 <matteo.federici96@gmail.com>
2024-04-19 15:30:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//scrivere programma che scrive in console da 1 a 100
+
+//passi da seguire :
+// definire il for con i tre componenti in parentesi
+//definire cosa esegue ad ogni ciclo(console log numero )
+// se i= multiplo 3 mi stampa fizz e non 3
+// definisco la condizione di multiplo di 3 
+//definisco la condizione di multiplo di 5 
+//definisco la condizione di quando è sia il caso 1 che 2 
+// definisco la condizione di quando non vengono soddisfatti i requisiti 1 e 2
+
+for(let i=1;i<=100;i= i+ 1){
+    
+     
+
+    if(i % 3 == 0) {
+        let i="fizz";
+        console.log(i);
+    }
+
+    else if(i % 5 ==0){
+        let i="buzz"
+        console.log(i);
+    }
+    else{
+        console.log(i)
+
+            
+    }
+
+}
